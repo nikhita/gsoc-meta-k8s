3b3bdf933a4b93c85151040ba90cd1937547b201
Nikhita Raghunath <nikitaraghunath@gmail.com>
2017-05-11 20:06:22 +0530

    Add script for setting kube-apiextensions-server

---
+++ notes/set-kube-apiextensions-server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Temporary steps to set up kube-apiextensions-server
+
+# this creates the image for the kube-apiextensions-server since it isn't published
+make WHAT=vendor/k8s.io/kube-apiextensions-server/ && vendor/k8s.io/kube-apiextensions-server/hack/build-image.sh
+
+# give yourself cluster-admin powers
+export KUBECONFIG=/var/run/kubernetes/admin.kubeconfig
+
+# creates a namespace for the kube-apiextensions-server pod
+kubectl create ns kube-apiextensions
+
+# creates the resources (permissions, RC, service, apiservice, etc) for the kube-apiextensions-server. This wont be necessary once we're embedded.
+kubectl create -f vendor/k8s.io/kube-apiextensions-server/artifacts/example/
+
+# create the APIService for mygroup.example.com. This wont be necessary once we're embedded.
+kubectl create -f vendor/k8s.io/kube-apiextensions-server/artifacts/customresource-01/noxu-apiservice.yaml
+
+# create a CustomResourceDefinition
+sudo kubectl create -f vendor/k8s.io/kube-apiextensions-server/artifacts/customresource-01/noxu-resource-definition.yaml
+
+# create an instance.
+sudo kubectl create -f vendor/k8s.io/kube-apiextensions-server/artifacts/customresource-01/noxu.yaml
